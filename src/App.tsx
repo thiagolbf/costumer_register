4531edc4b7028ce2809e1e788a1774b8e3e40eec
Thiago Barcelos <thiagolbf@hotmail.com>
2023-01-26 22:14:47 -0300

    adding globalstyle to use in all app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
+import { Globalstyle } from "./style/global";
+
 function App() {
-  return <h1>HELLO</h1>;
+  return (
+    <>
+      <Globalstyle />
+      <ToastContainer
+        position="top-left"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+      />
+
+      <h1>hello</h1>
+    </>
+  );
 }
 
 export default App;
